050cc6c557cca5dfa59e6316809be669ab075d5b
dex01612, Hauke Jaeger <hauke.jaeger@nterra.com>
2017-04-06 17:36:40 +0200

    TestMainJs script refactoring

--- kotlin-jasmine-maven-plugin/src/main/kotlin/com/github/crunc/kotlin/jasmine/maven/conf/TestMainJs.kt
+++ kotlin-jasmine-maven-plugin/src/main/kotlin/com/github/crunc/kotlin/jasmine/maven/conf/TestMainJs.kt
@@ -7,30 +7,29 @@ class TestMainJs(
         val karmaGlobal: String = "window['__karma__']",
         val requireGlobal: String = "require",
         val testFilePattern: String = "(spec|test|tests)\\.js\$",
-        val karmaBaseUrl: String = "/base"
+        val karmaBaseUrl: String = "/base",
+        val requireBaseUrl: String = "/"
 ) : Writable {
 
     private val script: String get() = """
 (function (karma, require) {
 
     var TEST_REGEXP = /$testFilePattern/i;
-    var allTestFiles = [];
+    var deps = [];
 
-    // Get a list of all the test files to include
+    function normalize(file) {
+      return file.replace(/^\$karmaBaseUrl\/|\.js$/g, '');
+    }
+
     Object.keys(karma.files).forEach(function (file) {
-
         if (TEST_REGEXP.test(file)) {
-
-            // Normalize paths to RequireJS module names.
-            // If you require sub-dependencies of test files to be loaded as-is (requiring file extension) then do not normalize the paths
-            var normalizedTestModule = file.replace(/^\$karmaBaseUrl\/|\.js$/g, '');
-            allTestFiles.push(normalizedTestModule);
+            deps.push(normalize(file));
         }
     });
 
     require.config({
-        baseUrl: '/',
-        deps: allTestFiles,
+        baseUrl: '$requireBaseUrl',
+        deps: deps,
         callback: karma.start
     });
 
